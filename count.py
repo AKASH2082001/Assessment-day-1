1ce9584bd5d38db75ecc4f64f37ac6ecf42ae358
akash2082001 <akashbme208@gmail.com>
2022-03-02 23:15:07 +0530

    Assessment day 1

---
+++ count.py
@@ -0,0 +1,5 @@
+a = input("enter any sentance")
+
+b= a.lower()
+
+print("count of A",b.count("a"),"count of E",b.count("e"),"count of I",b.count("i"),"count of o",b.count("o"),"count of u",b.count("u"))
